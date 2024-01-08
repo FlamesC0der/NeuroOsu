89098cd4b48bef37d4a44881341c9cd2b20811b3
Alexey <alexm20games@gmail.com>
2024-01-08 14:31:52 +0300

    upd6

--- AI/record_dataset.py
+++ AI/record_dataset.py
@@ -1,23 +1,39 @@
 import cv2
+import time
+import keyboard
 
 from settings import *
 from data.capture_screen import capture_screen
 from util.loader import save_data
 
+active = False
+
+
+def toogle():
+    global active
+    active = not active
+
+
+keyboard.add_hotkey("ctrl+shift+a", toogle)
+
 
 def main():
+    mode = input("select mode (t/v)")
+    path = TRAIN_DIR if "t" in mode else TEST_DIR
+    print(path)
     while True:
-        frame = capture_screen()
+        if active:
+            frame = capture_screen()
 
-        x, y = pyautogui.position()
+            x, y = pyautogui.position()
 
-        if x / w > 1:
-            continue
+            if x / w > 1:
+                continue
 
-        cv2.imshow('Computer vision', frame)
-        cv2.waitKey(1)
+            cv2.imshow('Computer vision', frame)
+            cv2.waitKey(1)
 
-        save_data(frame, x, y, w, h)
+            save_data(path, frame, x, y, w, h)
 
 
 if __name__ == "__main__":
--- AI/util/loader.py
+++ AI/util/loader.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import base64
+import os
 
 
 def load_data(path) -> list[dict]:
@@ -10,11 +11,13 @@ def load_data(path) -> list[dict]:
     return data
 
 
-def save_data(img: np.ndarray, x: float, y: float, w: int, h: int) -> None:
-    with open("dataset/cursor_data.csv", 'a') as f:
+def save_data(path: str, img: np.ndarray, x: float, y: float, w: int, h: int) -> None:
+    write_header = not os.path.exists(path) or os.stat(path).st_size == 0
+    with open(path, 'a') as f:
         fieldnames = ['img', 'x', 'y']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
-        print("save")
-        # if len(writer) == 0:
-        #     writer.writeheader()
+        print("save", x, y, x / w, y / h)
+
+        if write_header:
+            writer.writeheader()
         writer.writerow({'img': base64.b64encode(img.tobytes()).decode('utf-8'), 'x': x / w, 'y': y / h})
